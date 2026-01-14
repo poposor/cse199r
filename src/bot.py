4e33b390db320ccf975d355ebe703c853746bc1b
Caden Reeder <cadenreeder@gmail.com>
2026-01-14 10:37:17 -0700

    Knarr drives toward boostpad 3

--- src/bot.py
+++ src/bot.py
@@ -1,6 +1,9 @@
 from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
 from rlbot.utils.structures.game_data_struct import GameTickPacket
 
+from util.drive import steer_toward_target
+from util.vec import Vec3
+
 class Knarr(BaseAgent):
 
     def __init__(self, name, team, index):
@@ -18,12 +21,15 @@ class Knarr(BaseAgent):
         field = self.get_field_info()
         target = field.boost_pads[self.targetBoostPad]
 
-        my_location = my_car.physics.location
-        target_location = target.location
+        my_location = Vec3(my_car.physics.location)
+        target_location = Vec3(target.location)
 
         print("Car location: ", my_location)
         print("Target location: ", target_location)
-        self.renderer.draw_string_3d(my_location, 1, 1, f'{my_location} : {target_location}', self.renderer.white())
+        # self.renderer.draw_string_3d(my_location, 1, 1, f'{my_location} : {target_location}', self.renderer.white())
+        self.renderer.draw_string_3d(my_location, 1, 1, 'Knarr', self.renderer.white())
+
+        controller.steer = steer_toward_target(my_car, target_location)
 
         controller.throttle = 0.1
 
